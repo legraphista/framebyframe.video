3791dfdc4d0522436facc5b7b3a4d47bf22fe4ea
Stefan-Gabriel Muscalu <stefan.gabriel.muscalu@gmail.com>
2019-05-06 22:38:28 +0300

    Update: dev build source maps

--- webpack.config.js
+++ webpack.config.js
@@ -74,7 +74,7 @@ var options = {
 };
 
 if (env.NODE_ENV === "development") {
-  options.devtool = "cheap-module-eval-source-map";
+  options.devtool = "cheap-module-source-map";
 }
 
 module.exports = options;
